4a09e56a11177cf9421c0287e3a091465de390e5
Alireza <noorbakhsha1@gmail.com>
2025-11-19 16:53:27 +0330

    initial WeatherApi with WebService with hardcoded endpoint

---
+++ Program.cs
@@ -0,0 +1,55 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/weather/{city}", (string city) =>
+{
+    var response = new WeatherResponse(
+        City: city,
+        TemperatureC: 15,
+        TemperatureF: 50,
+        Summery: "Cold AF"
+    );
+
+    return Results.Ok(response);
+}).WithName("GetWeatherForCity");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public record WeatherResponse(string City, int TemperatureC, int TemperatureF, string Summery);
---
+++ WeatherService.cs
@@ -0,0 +1,16 @@
+﻿public class WeatherService
+{
+    public WeatherResponse GetCurrentWeather(string city)
+    {
+        var temperatureC = 15;
+        var temperatureF = 30;
+        var summery = "cold AF";
+
+        return new WeatherResponse(
+            City: city,
+            TemperatureC: temperatureC,
+            TemperatureF: temperatureF,
+            Summery: summery
+        );
+    }
+}
